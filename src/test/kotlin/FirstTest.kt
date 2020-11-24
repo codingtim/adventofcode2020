9da5a8faff67c2bc1508e0fb7d1cb308ba641693
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-11-24 10:31:05 +0100

    Initial commit

---
+++ src/test/kotlin/FirstTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class FirstTest {
+
+    @Test
+    internal fun test_one() {
+        assertTrue(true)
+    }
+}
